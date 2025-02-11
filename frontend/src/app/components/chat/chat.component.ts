9938ff7563e916711befb0bc7cf9c9ee0c22a7c3
LSVTECH <140634316+LSVTECH@users.noreply.github.com>
2025-02-11 10:57:12 -0500

    remove unused comment

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MarkdownModule } from 'ngx-markdown'; // Importa MarkdownModule
+import { MarkdownModule } from 'ngx-markdown';
 import { ChatService } from '../../services/chat.service';
 import { ChatMessage } from '../../models/chat-message';
 import { parseChatResponse } from '../../../utils/parseChatResponse';
